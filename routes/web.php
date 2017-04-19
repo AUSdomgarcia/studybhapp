8e52f3ba3156c8c9153d6ee1ce449ee667bf566a
bb_domgarcia <dom.garcia@nuworks.ph>
2017-04-19 19:54:33 +0800

    changes

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,31 @@
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-Route::get('/home', 'HomeController@index');
 
-Route::group(['middleware' => ['web']], function() {
-	Route::get("roles/create", "RolesController@store");
+/*
+|--------------------------------------------------------------------------
+| Cms Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/login', function(){
+	return redirect('/admin/login');
+});
+
+Route::group(['prefix' => 'admin'], function(){
+	Auth::routes();
+	
+	Route::group(['middleware' => 'auth'], function(){
+		Route::get('/', function() {
+			return redirect('/admin/login');
+		});
+		Route::get('/dashboard', function() { 
+			return view('home');
+		});
+	});
 });
